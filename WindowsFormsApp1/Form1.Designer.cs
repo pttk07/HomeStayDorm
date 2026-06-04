cef8d37c022108d2b9f79edaa9b54f4de8286709
NITRO <NITRO@LAPTOP-GD3GRD7A>
2026-06-04 22:17:41 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblRoomType = new System.Windows.Forms.Label();
+            this.cboRoomType = new System.Windows.Forms.ComboBox();
+            this.lblCheckIn = new System.Windows.Forms.Label();
+            this.dtpCheckIn = new System.Windows.Forms.DateTimePicker();
+            this.lblCheckOut = new System.Windows.Forms.Label();
+            this.dtpCheckOut = new System.Windows.Forms.DateTimePicker();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(107, 25);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(463, 29);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "PHIẾU ĐĂNG KÝ PHÒNG KHÁCH SẠN";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(67, 98);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(67, 16);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Họ và tên:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(200, 95);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(345, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(67, 148);
+            this.lblPhone.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(88, 16);
+            this.lblPhone.TabIndex = 3;
+            this.lblPhone.Text = "Số điện thoại:";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(200, 144);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(345, 22);
+            this.txtPhone.TabIndex = 4;
+            // 
+            // lblRoomType
+            // 
+            this.lblRoomType.AutoSize = true;
+            this.lblRoomType.Location = new System.Drawing.Point(67, 197);
+            this.lblRoomType.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRoomType.Name = "lblRoomType";
+            this.lblRoomType.Size = new System.Drawing.Size(77, 16);
+            this.lblRoomType.TabIndex = 5;
+            this.lblRoomType.Text = "Loại phòng:";
+            // 
+            // cboRoomType
+            // 
+            this.cboRoomType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboRoomType.FormattingEnabled = true;
+            this.cboRoomType.Items.AddRange(new object[] {
+            "Phòng Tiêu Chuẩn (Standard)",
+            "Phòng Cao Cấp (Superior)",
+            "Phòng Sang Trọng (Deluxe)",
+            "Phòng Tổng Thống (VIP)"});
+            this.cboRoomType.Location = new System.Drawing.Point(200, 193);
+            this.cboRoomType.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cboRoomType.Name = "cboRoomType";
+            this.cboRoomType.Size = new System.Drawing.Size(345, 24);
+            this.cboRoomType.TabIndex = 6;
+            // 
+            // lblCheckIn
+            // 
+            this.lblCheckIn.AutoSize = true;
+            this.lblCheckIn.Location = new System.Drawing.Point(67, 246);
+            this.lblCheckIn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCheckIn.Name = "lblCheckIn";
+            this.lblCheckIn.Size = new System.Drawing.Size(116, 16);
+            this.lblCheckIn.TabIndex = 7;
+            this.lblCheckIn.Text = "Ngày nhận phòng:";
+            // 
+            // dtpCheckIn
+            // 
+            this.dtpCheckIn.CustomFormat = "dd/MM/yyyy";
+            this.dtpCheckIn.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpCheckIn.Location = new System.Drawing.Point(200, 239);
+            this.dtpCheckIn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpCheckIn.Name = "dtpCheckIn";
+            this.dtpCheckIn.Size = new System.Drawing.Size(159, 22);
+            this.dtpCheckIn.TabIndex = 8;
+            // 
+            // lblCheckOut
+            // 
+            this.lblCheckOut.AutoSize = true;
+            this.lblCheckOut.Location = new System.Drawing.Point(373, 246);
+            this.lblCheckOut.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCheckOut.Name = "lblCheckOut";
+            this.lblCheckOut.Size = new System.Drawing.Size(102, 16);
+            this.lblCheckOut.TabIndex = 9;
+            this.lblCheckOut.Text = "Ngày trả phòng:";
+            // 
+            // dtpCheckOut
+            // 
+            this.dtpCheckOut.CustomFormat = "dd/MM/yyyy";
+            this.dtpCheckOut.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpCheckOut.Location = new System.Drawing.Point(493, 239);
+            this.dtpCheckOut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpCheckOut.Name = "dtpCheckOut";
+            this.dtpCheckOut.Size = new System.Drawing.Size(159, 22);
+            this.dtpCheckOut.TabIndex = 10;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(200, 308);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(133, 43);
+            this.btnRegister.TabIndex = 11;
+            this.btnRegister.Text = "Đăng Ký";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(373, 308);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(133, 43);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Hủy Bỏ";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 394);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.dtpCheckOut);
+            this.Controls.Add(this.lblCheckOut);
+            this.Controls.Add(this.dtpCheckIn);
+            this.Controls.Add(this.lblCheckIn);
+            this.Controls.Add(this.cboRoomType);
+            this.Controls.Add(this.lblRoomType);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Quản Lý Khách Sạn";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.HelpRequested += new System.Windows.Forms.HelpEventHandler(this.Form1_HelpRequested);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblRoomType;
+        private System.Windows.Forms.ComboBox cboRoomType;
+        private System.Windows.Forms.Label lblCheckIn;
+        private System.Windows.Forms.DateTimePicker dtpCheckIn;
+        private System.Windows.Forms.Label lblCheckOut;
+        private System.Windows.Forms.DateTimePicker dtpCheckOut;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
